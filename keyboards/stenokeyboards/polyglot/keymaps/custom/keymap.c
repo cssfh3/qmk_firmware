1bbc701e2e6cb31a4e4a03e412cbe6ae363d791d
cssfh3 <cssfh3@gmail.com>
2025-05-30 17:41:09 -0500

    Added custom directory

---
+++ keyboards/stenokeyboards/polyglot/keymaps/custom/keymap.c
@@ -0,0 +1,137 @@
+/*
+Copyright 2023 StenoKeyboards
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+//#include "keymap_steno.h"
+
+enum polyglot_layers {
+    _UNI_GEMINI,
+    _QWERTY,
+    _COLEMAK,
+    _RAISE,
+    _LOWER,
+};
+
+enum polyglot_keycodes {
+    UNI_GEMINI = SAFE_RANGE,
+    QWERTY,
+    COLEMAK,
+    RAISE,
+    LOWER,
+    BACK,
+};
+
+int default_keymap = _COLEMAK;
+
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+   [_UNI_GEMINI] = LAYOUT_split_3x6_3(
+   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       XXXXXXX, STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, 					  STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,
+   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       XXXXXXX, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, 					 STN_ST4,  STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
+   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        BACK,  KC_LSFT,  KC_LCTL,  KC_LALT,  KC_LCMD,  KC_SPC,    				KC_VOLD,  KC_LEFT, KC_DOWN, KC_UP ,KC_RIGHT,  KC_VOLU,
+   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                   STN_N1,  STN_A,   STN_O,                       STN_E,   STN_U, STN_N2
+                                       //`--------------------------'  `--------------------------'
+
+   ),
+
+  [_RAISE] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+   COLEMAK,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,KC_7,KC_8,KC_9,    KC_0, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+   QWERTY,   _______, KC_LEFT, KC_DOWN,  KC_UP,KC_RIGHT,                        KC_CAPS_LOCK,KC_KP_4,KC_KP_5, KC_KP_6,  _______, _______,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      UNI_GEMINI,  _______,KC_DEL, _______, _______, _______,                      _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                _______, _______, _______,                      _______, KC_KP_0, KC_KP_DOT
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [_LOWER] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,               KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+     _______, RGB_VAD, RGB_VAI, RGB_HUD, RGB_HUI, RGB_MODE_FORWARD,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          _______,XXXXXXX, _______,    _______, _______, _______
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [_QWERTY] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      UNI_GEMINI,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LGUI,   RAISE, KC_LSFT,       KC_SPC, LOWER, KC_RALT
+                                      //`--------------------------'  `--------------------------'
+
+  ),
+
+    [_COLEMAK] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_BSPC,LGUI_T(KC_A),LALT_T(KC_R),LCTL_T(KC_S),LSFT_T(KC_T),KC_D,        KC_H, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_ESC,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_ENT,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                        KC_LGUI,   RAISE, KC_LSFT,       KC_SPC, LOWER, KC_RALT
+                                      //`--------------------------'  `--------------------------'
+
+  ),
+
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+		    layer_move(_QWERTY);
+        default_keymap = _QWERTY;
+      }
+      return false;
+    case COLEMAK:
+      if (record->event.pressed) {
+		    layer_move(_COLEMAK);
+        default_keymap = _COLEMAK;
+      }
+      return false;
+    case UNI_GEMINI:
+      if (record->event.pressed) {
+        layer_move(_UNI_GEMINI);
+      }
+      return false;
+    case BACK:
+      if (record->event.pressed) {
+        layer_move(default_keymap);
+      }
+      return false;
+  }
+  return true;
+}
